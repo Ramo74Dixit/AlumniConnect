c464b966745c8c6660eab48b112121998755f90e
Ankush <ankush.spencer015@gmail.com>
2024-08-22 10:04:52 +0530

    .env added

--- Backend/db/connection.js
+++ Backend/db/connection.js
@@ -13,12 +13,14 @@
 
 // db/connection.js
 
+require('dotenv').config();
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://ankushspencer015:9918919442@cluster0.sffpm.mongodb.net/alumni-portal?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
 mongoose.connect(uri, {
-
+  // Add your options here if needed
 })
 .then(() => console.log("Mongoose connected to MongoDB Atlas"))
 .catch((error) => console.error("Mongoose connection error:", error));
+
